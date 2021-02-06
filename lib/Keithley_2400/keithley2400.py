573b598b870b8e99bf4f9043bfbb0fac427bfecb
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-02-06 22:19:35 +0100

    added send get msg

--- lib/Keithley_2400/keithley2400.py
+++ lib/Keithley_2400/keithley2400.py
@@ -20,3 +20,19 @@ class KEITHLEY2400:
             self.serial_con.close()
         except:
             raise Exception('Error: Could not close connection')
+
+    '''SEND GET MSG'''
+    def send_msg(self, msg, receive):
+        try:
+            self.serial_con.reset_input_buffer()
+            self.serial_con.reset_output_buffer()
+
+            self.serial_con.write(msg)
+            self.serial_con.flush()
+
+            if receive is True:
+                data = self.serial_con.read_until('\n', BUFFER)
+                data = str(data, 'utf-8')
+                return data
+        except:
+            raise Exception('Error: Writing and/or reading data')
