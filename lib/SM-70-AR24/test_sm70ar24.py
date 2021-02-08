9d341f29cbfe55a3d6ddd82b2b2aafa8abea0a90
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-02-08 14:38:53 +0100

    tested device

--- lib/SM-70-AR24/test_sm70ar24.py
+++ lib/SM-70-AR24/test_sm70ar24.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
 
     volt = sm.FUNC_VOLT
     curr = sm.FUNC_CURR
-    pwr  = sm.FUNC_PWR
 
     try:
         device = sm.SM70AR24(IP_ADD, PORT)
@@ -29,32 +28,6 @@ if __name__ == '__main__':
     except Exception as e:
         print(e)
     time.sleep(1)
-
-    print('set_max_out(volt, 70): ')
-    try:
-        device.set_max_out(volt, 70)
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-
-    print('get_max_out(volt): ')
-    try:
-        print(device.get_max_out(volt))
-    except Exception as e:
-        print(e)
-
-    print('set_max_value(curr, 24): ')
-    try:
-        device.set_max_out(curr, 24)
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-
-    print('get_max_value(curr): ')
-    try:
-        print(device.get_max_out(curr))
-    except Exception as e:
-        print(e)
 
     print('\n-------------------------------\nVOLTAGE\n------------------------------\n')
 
@@ -107,32 +80,6 @@ if __name__ == '__main__':
         print(e)
     time.sleep(1)
 
-    print('set_max_out(volt, 70): ')
-    try:
-        device.set_max_out(volt, 70)
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-
-    print('get_max_out(volt): ')
-    try:
-        print(device.get_max_out(volt))
-    except Exception as e:
-        print(e)
-
-    print('set_max_value(curr, 24): ')
-    try:
-        device.set_max_out(curr, 24)
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-
-    print('get_max_value(curr): ')
-    try:
-        print(device.get_max_out(curr))
-    except Exception as e:
-        print(e)
-
     print('\n-------------------------------\nCURRENT\n------------------------------\n')
 
     print('set_value(curr, 2): ')
@@ -184,54 +131,6 @@ if __name__ == '__main__':
         print(e)
     time.sleep(1)
 
-    print('volt_max_out = 70: ')
-    try:
-        device.volt_max_out = 70
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-    
-    print('volt_max_out: ')
-    try:
-        print(device.volt_max_out)
-    except Exception as e:
-        print(e)
-
-    print('curr_max_out = 24: ')
-    try:
-        device.curr_max_out = 24
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-
-    print('curr_max_out: ')
-    try:
-        print(device.curr_max_out)
-    except Exception as e:
-        print(e)
-
-    print('\n-------------------------------\nPOWER\n------------------------------\n')
-
-    print('volt_out = 24: ')
-    try:
-        device.volt_out = 20
-    except Exception as e:
-        print(e)
-    time.sleep(1)
-
-    print('meas(pwr): ')
-    try:
-        print(device.meas(pwr))
-    except Exception as e:
-        print(e)
-
-    print('\nPROPERTY\n')
-
-    print('pwr: ')
-    try:
-        print(device.pwr)
-    except Exception as e:
-        print(e)
 
     try:
         device.close()
