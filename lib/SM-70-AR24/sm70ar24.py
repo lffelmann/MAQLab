f9bdbab2b42dc350bc26d4d45a4298a788f00aaa
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-02-08 14:00:35 +0100

    deleted amx output set check

--- lib/SM-70-AR24/sm70ar24.py
+++ lib/SM-70-AR24/sm70ar24.py
@@ -152,8 +152,6 @@ class SM70AR24:
     # set output voltage and current
     def set_out(self, func, value):
         try:
-            if self.check_max_value() is False:                                     # check if max output is set
-                raise Exception('Error: Max output is not set')
             if self.check_func('set', func) is False:                               # check if func is available
                 raise Exception('Error: Function is unavailable')
             if self.check_value(func, value) is False:                              # check if value is available
@@ -185,8 +183,6 @@ class SM70AR24:
     # measure voltage, current and power
     def meas(self, func):
         try:
-            if self.check_max_value() is False:                                     # check if max output is set
-                raise Exception('Error: Max output is not set')
             if self.check_func('meas', func) is False:                              # check if func is available
                 raise Exception('Error: Function is unavailable')
 
@@ -227,16 +223,6 @@ class SM70AR24:
             elif func == FUNC_CURR and 0 <= value <= self.value_max_curr:
                 value_ok = True
             return value_ok
-        except:
-            return False
-
-    # check if max value is set
-    def check_max_value(self):
-        try:
-            max_val = False
-            if self.max_volt == True and self.max_curr == True:
-                max_val = True
-            return max_val
         except:
             return False
 
