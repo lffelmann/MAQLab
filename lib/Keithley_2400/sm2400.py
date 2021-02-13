540edd84b6ebd30c1fa52a110a0ddd7c221df9d4
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-02-13 19:41:27 +0100

    added output and source mode

--- lib/Keithley_2400/sm2400.py
+++ lib/Keithley_2400/sm2400.py
@@ -19,6 +19,8 @@ DEFAULT_BAUDRATE = 9600
 ON = 'on'
 OFF = 'off'
 
+VOLT = 'volt'
+CURR = 'curr'
 
 class SM2400:
 
@@ -97,6 +99,64 @@ class SM2400:
         except:
             raise
 
+    '''OUTPUT'''
+    # set output
+    def set_out(self, state):
+        try:
+            if self.check_state(state) is False:                                    # check if state is available
+                raise Exception('Error: State is unavailable')
+
+            array_state = self.convert_state(state)                                 # convert state to array for msg
+
+            msg = bytearray(':OUTP ' + array_state + '\r\n', 'utf-8')               # set output
+            self.send_msg(msg, False)
+        except:
+            raise
+
+    # get output
+    def get_out(self):
+        try:
+            msg = bytearray(':OUTP?\r\n', 'utf-8')                                  # get output
+            data = self.send_msg(msg, True)
+
+            if data == '1':                                                         # if output is on -> return on
+                return ON
+            elif data == '0':                                                       # if output is off -> return off
+                return OFF
+        except:
+            raise
+
+    # -----------------------------------------------------------------------
+    # SOURCE
+    # -----------------------------------------------------------------------
+
+    '''SOURCE MODE'''
+    # set source mode
+    def set_sour_mode(self, mode):
+        try:
+            if self.check_sour_mode(mode) is False:                             # check if source mode is available
+                raise Exception('Error: Mode is unavailable')
+
+            array_mode = self.convert_sour_mode(mode)                           # convert mode to array for msg
+
+            msg = bytearray(':SOUR:FUNC ' + array_mode + '\r\n', 'utf-8')       # set source mode
+            self.send_msg(msg, False)
+        except:
+            raise
+
+    #get source mode
+    def get_sour_mode(self):
+        try:
+            msg = bytearray(':SOUR:FUNC?\r\n', 'utf-8')                         # get source mode
+            data = self.send_msg(msg, True)
+
+            if data == 'VOLT':                                                  # if source mode is volt -> return volt
+                return VOLT
+            elif data == 'CURR':                                                # if source mode is curr -> return curr
+                return CURR
+        except:
+            raise
+
     # -----------------------------------------------------------------------
     # CHECK/CONVERT
     # -----------------------------------------------------------------------
@@ -112,17 +172,38 @@ class SM2400:
         except:
             return False
 
+    # check if source mode is available
+    def check_sour_mode(self, mode):
+        try:
+            mode_ok = False
+            if mode == VOLT or mode == CURR:
+                mode_ok = True
+            return mode_ok
+        except:
+            return False
+
     '''CONVERT'''
     # convert state to array
     def convert_state(self, state):
         try:
             if state == ON:
-                array_state = 'ON'
+                array_state = '1'
             elif state == OFF:
-                array_state = 'OFF'
+                array_state = '0'
             return array_state
         except:
             raise Exception('Error: Could not convert state')
+
+    # convert source mode to array
+    def convert_sour_mode(self, mode):
+        try:
+            if mode == VOLT:
+                array_mode = 'VOLT'
+            elif mode == CURR:
+                array_mode = 'CURR'
+            return array_mode
+        except:
+            raise Exception('Error: Could not convert source mode')
 
     # -----------------------------------------------------------------------
     # PROPERTY
@@ -142,6 +223,38 @@ class SM2400:
         except:
             raise
 
+    # output
+    def _get_out(self):
+        try:
+            return self.get_out()
+        except:
+            raise
+
+    def _set_out(self, state):
+        try:
+            self.set_out(state)
+        except:
+            raise
+
+    # source mode
+    def _get_sour_mode(self):
+        try:
+            return self.get_sour_mode()
+        except:
+            raise
+
+    def _set_sour_mode(self, mode):
+        try:
+            self.set_sour_mode(mode)
+        except:
+            raise
+
     '''PROPERTY'''
     # display
     display = property(_get_display, _set_display)
+
+    # output
+    output = property(_get_out, _set_out)
+
+    # source mode
+    sour_mode = property(_get_sour_mode, _set_sour_mode)
